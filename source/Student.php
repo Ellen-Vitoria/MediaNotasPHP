2b1159b8c89a9704da8f1095a7fdf3e52867597f
ellen <ellen.vitoria.ogs@gmail.com>
2020-10-16 21:08:38 -0300

    Criando atributos e métodos na classe do Estudante

---
+++ source/Student.php
@@ -0,0 +1,59 @@
+<?php
+
+class Student
+{
+    private $firstName;
+    private $lastName;
+    private $grade1;
+    private $grade2;
+    private $grade3;
+    private $grade4;
+
+    public function getFirstName(){
+       return $this->firstName;
+    }
+
+    public function setFirstName($fname){
+        $this->firstname = $fname;
+    }
+
+    public function getLastName(){
+        return $this->lastName;
+    }
+
+    public function setLastName($lname){
+        $this->lastName = $lname;
+    }
+
+    public function getGrade1(){
+        return $this->grade1;
+    }
+
+    public function setGrade1($grade1){
+        $this->grade1 = $grade1;
+    }
+
+    public function getGrade2(){
+        return $this->grade2;
+    }
+
+    public function setGrade2($grade2){
+        $this->grade2 = $grade2;
+    }
+
+    public function getGrade3(){
+        return $this->grade3;
+    }
+
+    public function setGrade3($grade3){
+        $this->grade3 = $grade3;
+    }
+    
+    public function getGrade4(){
+        return $this->grade4;
+    }
+
+    public function setGrade4($grade4){
+        $this->grade4 = $grade4;
+    }
+}
